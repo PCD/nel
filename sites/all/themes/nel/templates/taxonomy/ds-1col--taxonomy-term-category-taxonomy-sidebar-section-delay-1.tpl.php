a67db0648b9475ac0ecd7064e47ea6e8169de9e4
Cristian Cienfuegos <cristian100@gmail.com>
2014-07-29 02:22:43 -0600

    added more templates

---
+++ sites/all/themes/nel/templates/taxonomy/ds-1col--taxonomy-term-category-taxonomy-sidebar-section-delay-1.tpl.php
@@ -0,0 +1,24 @@
+<?php
+// Articles
+$articles = views_embed_view('taxonomy_articles_sidebar','block_1', $tid);
+
+/**
+ * @file
+ * Display Suite 1 column template.
+ */
+?>
+<<?php print $ds_content_wrapper; print $layout_attributes; ?> class="ds-1col <?php print $classes;?> clearfix">
+
+  <?php if (isset($title_suffix['contextual_links'])): ?>
+  <?php print render($title_suffix['contextual_links']); ?>
+  <?php endif; ?>
+
+  <?php print $ds_content; ?>
+  <div class="article-content">
+    <?php print $articles;?>
+  </div>
+</<?php print $ds_content_wrapper ?>>
+
+<?php if (!empty($drupal_render_children)): ?>
+  <?php print $drupal_render_children ?>
+<?php endif; ?>

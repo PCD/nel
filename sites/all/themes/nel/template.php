8f1a24d736f91426936cefdb270245f396f9a42e
Cristian Cienfuegos <cristian100@gmail.com>
2014-08-21 20:15:12 -0600

    change

--- sites/all/themes/nel/template.php
+++ sites/all/themes/nel/template.php
@@ -5,3 +5,33 @@
  * Template overrides as well as (pre-)process and alter hooks for the
  * Nayarit En Linea theme.
  */
+
+
+/**
+ * Overrides Breadcrumb.
+ */
+function nel_breadcrumb(&$vars) {
+  if ( empty($vars['breadcrumb']) ) {
+    return '';
+  }
+  
+  $breadcrumb = $vars['breadcrumb'];
+  $count = count($breadcrumb);
+  
+   // Build output
+  $output = "<ul class=\"breadcrumb\">\n";
+  
+  $items = array();
+   // Add items
+  foreach($breadcrumb as $i => $crumb) {
+    $crumb = str_replace('&lt;br/&gt;', ' ', $crumb);
+    $odd = $i%2==0?'even':'odd';
+    $order = $i==0?' home':($i==($count-1)?' last':'');
+    $class = "item {$odd}{$order}";
+    $items[] = "<li class=\"{$class}\">{$crumb}</li>";
+  }
+  $output .= implode("<li class=\"separator\">&raquo;</li>", $items);
+
+  $output .= "</ul>\n";
+  return $output;
+}

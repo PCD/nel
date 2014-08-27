f79c470739ea1b7bf3072d86a4c633d115c8f142
Cristian Cienfuegos <cristian100@gmail.com>
2014-08-27 17:09:10 -0600

    changes

---
+++ customscripts/mpcUpdateStats.php
@@ -0,0 +1,6 @@
+<?php
+define('DRUPAL_ROOT', getcwd());
+$_SERVER['REMOTE_ADDR'] = "localhost"; // Necessary if running from command line
+require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
+drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+mpc_data_update_stats();

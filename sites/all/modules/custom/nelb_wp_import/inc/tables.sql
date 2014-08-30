ab2bfe160aa59e76380b471db866551277226cdc
Cristian Cienfuegos <cristian100@gmail.com>
2014-08-29 22:41:12 -0600

    new

---
+++ sites/all/modules/custom/nelb_wp_import/inc/tables.sql
@@ -0,0 +1,18 @@
+CREATE TABLE nelb_file (
+  source_id INTEGER UNSIGNED NOT NULL, 
+  source_blog INTEGER UNSIGNED NOT NULL, 
+  guid VARCHAR(255) NOT NULL, 
+  fid INTEGER UNSIGNED NOT NULL, 
+  FOREIGN KEY (fid) REFERENCES file_managed(fid) ON DELETE CASCADE, 
+  PRIMARY KEY (source_id, source_blog)
+);
+
+CREATE TABLE nelb_post (
+  source_id INTEGER UNSIGNED NOT NULL, 
+  source_blog INTEGER UNSIGNED NOT NULL, 
+  guid VARCHAR(255) NOT NULL, 
+  nid INTEGER UNSIGNED NOT NULL, 
+  FOREIGN KEY (nid) REFERENCES node(nid) ON DELETE CASCADE, 
+  PRIMARY KEY (source_id, source_blog)
+);
+

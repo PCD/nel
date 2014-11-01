082b2145240714a05e2d726566d00f45a9d96a69
Cristian Gonzalez <cristian100@gmail.com>
2014-11-01 10:23:15 -0700

    added blank target

--- sites/all/modules/custom/socialcounts/themes/socialcounts-youtube.tpl.php
+++ sites/all/modules/custom/socialcounts/themes/socialcounts-youtube.tpl.php
@@ -1,3 +1,3 @@
 <a class="social-icon" target="_blank" href="http://www.youtube.com/subscription_center?add_user=jartello">YouTube</a>
 <a class="social-button" target="_blank" href="http://www.youtube.com/subscription_center?add_user=jartello">Suscr&iacute;bete</a>
-<div class="social-stats"><span id="youtubeSubscribers"></span> Suscriptores</div>
+<div class="social-stats"><span id="youtubeSubscribers">24,418</span> Suscriptores</div>

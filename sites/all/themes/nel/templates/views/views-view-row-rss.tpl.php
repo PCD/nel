df539742c0187452192c2ae384c8c8ceed409241
Victor Polanco Plata <vipopmx@gmail.com>
2015-02-22 14:08:58 -0700

    added  html sanitization for the feed page

---
+++ sites/all/themes/nel/templates/views/views-view-row-rss.tpl.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @file
+ * Default view template to display a item in an RSS feed.
+ *
+ * @ingroup views_templates
+ */
+ $description = strip_tags(html_entity_decode($description));
+ $description = preg_replace('/\s+/msi', ' ', $description);
+?>
+  <item>
+    <title><?php print $title; ?></title>
+    <link><?php print $link; ?></link>
+    <description><?php print $description; ?></description>
+    <?php print $item_elements; ?>
+  </item>

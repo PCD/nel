ea6d4b23a426337dc480e2095453b2954b3f7784
cristian100 <cristian100@gmail.com>
2015-09-30 01:53:53 -0600

    changed ads to rectangle

--- sites/all/modules/custom/nelads/templates/nelads-article_detail.tpl.php
+++ sites/all/modules/custom/nelads/templates/nelads-article_detail.tpl.php
@@ -4,7 +4,7 @@
      style="display:block"
      data-ad-client="ca-pub-6659210167862789"
      data-ad-slot="3150408355"
-     data-ad-format="auto"></ins>
+     data-ad-format="rectangle"></ins>
 <script>
 (adsbygoogle = window.adsbygoogle || []).push({});
 </script>

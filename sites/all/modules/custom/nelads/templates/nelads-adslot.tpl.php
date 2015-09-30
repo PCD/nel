86c01834a9f08f248845c78649eca555d59553b9
cristian100 <cristian100@gmail.com>
2015-09-30 17:36:43 -0600

    added new block for ads at header

---
+++ sites/all/modules/custom/nelads/templates/nelads-adslot.tpl.php
@@ -0,0 +1,3 @@
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script type="text/javascript">!function(a){Drupal.behaviors.neladsAdslot<?php print $adslot;?>={attach:function(d,o){a(".adslot_<?php print $adslot;?>",d).once("nelads",function(){var d=o.nelads.<?php print $adsvar;?>[Math.floor(Math.random()*o.nelads.<?php print $adsvar;?>.length)];a(".adsbygoogle.adslot_<?php print $adslot;?>").attr({"data-ad-client":d.ad_client,"data-ad-slot":d.ad_slot,"data-ad-format":d.ad_format}),(adsbygoogle=window.adsbygoogle||[]).push({})})}}}(jQuery);</script>
+<ins class="adsbygoogle adslot_<?php print $adslot;?>" style="display:block"></ins>
--- sites/all/modules/custom/nelads/templates/nelads-article_detail.tpl.php
+++
@@ -1,3 +0,0 @@
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<script type="text/javascript">!function(a){Drupal.behaviors.neladsAdslot1={attach:function(d,o){a(".adslot_1",d).once("nelads",function(){var d=o.nelads.ads[Math.floor(Math.random()*o.nelads.ads.length)];a(".adsbygoogle.adslot_1").attr({"data-ad-client":d.ad_client,"data-ad-slot":d.ad_slot,"data-ad-format":d.ad_format}),(adsbygoogle=window.adsbygoogle||[]).push({})})}}}(jQuery);</script>
-<ins class="adsbygoogle adslot_1" style="display:block"></ins>

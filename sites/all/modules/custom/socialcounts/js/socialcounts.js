a5af3707659f2df9a6ee4fef99caa4c3b5d0a70f
cristian100 <cristian100@gmail.com>
2015-09-30 20:24:33 -0600

    fix for social counts

--- sites/all/modules/custom/socialcounts/js/socialcounts.js
+++ sites/all/modules/custom/socialcounts/js/socialcounts.js
@@ -3,9 +3,10 @@
     attach: function (context, settings) {
       $('#block-socialcounts-counts', context).once('sc', function () {
         // Facebook
-        var url = "https://www.facebook.com/292496132255";
-        $.getJSON("http://graph.facebook.com/" + url, function (json){
-          $('#facebookCount').html(number_format(json.likes));
+        var url = "https://graph.facebook.com/fql?q=SELECT%20url,%20normalized_url,%20share_count,%20like_count,%20comment_count,%20total_count,commentsbox_count,%20comments_fbid,%20click_count%20FROM%20link_stat%20WHERE%20url=%20%27https://www.facebook.com/nayaritenlinea.mx%27";
+        //var url = "https://www.facebook.com/292496132255";
+        $.getJSON(url, function (json){
+          $('#facebookCount').html(number_format(json.data[0].like_count));
         });
         
         // Twitter

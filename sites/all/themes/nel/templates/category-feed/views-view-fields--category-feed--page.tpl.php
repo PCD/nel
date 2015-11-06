a0991dfdac3b26266130631bbe377f96f37782fe
cristian100 <cristian100@gmail.com>
2015-11-05 21:18:52 -0700

    fixed typo

--- sites/all/themes/nel/templates/category-feed/views-view-fields--category-feed--page.tpl.php
+++ sites/all/themes/nel/templates/category-feed/views-view-fields--category-feed--page.tpl.php
@@ -5,7 +5,7 @@
     $parent = array_shift($parents);
   }
   if ( isset($term->field_feed_logo[LANGUAGE_NONE][0]['fid']) ) {
-    $image_url = image_style_url('feed_logo', $term->field_feed_logo[LANGUAGE_NONE][0]['fid']);
+    $image_url = image_style_url('feed_logo', $term->field_feed_logo[LANGUAGE_NONE][0]['uri']);
     $dimensions = array(
       'width' => $term->field_feed_logo[LANGUAGE_NONE][0]['width'],
       'height' => $term->field_feed_logo[LANGUAGE_NONE][0]['height'],
@@ -13,6 +13,7 @@
     image_style_transform_dimensions('feed_logo', $dimensions);
     $image_width = $dimensions['width'];
     $image_height = $dimensions['height'];
+    $image_type = $term->field_feed_logo[LANGUAGE_NONE][0]['filemime'];
   }
 ?>
   <title><?php print check_plain($term->name);?></title>
@@ -22,5 +23,5 @@
     <source url="<?php print url('taxonomy/term/' . $parent->tid, array('absolute'=>TRUE));?>"><?php print check_plain($parent->name);?></source>
 <?php endif;?>
 <?php if (isset($image_url)):?>
-    <media:content url="<?php print $image_url;?>" width="<?php print $image_width;?>" height="<?php print $image_height;?>" />
+    <media:content url="<?php print $image_url;?>" width="<?php print $image_width;?>" height="<?php print $image_height;?>" type="<?php print $image_type;?>" />
 <?php endif; ?>

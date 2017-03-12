8853bbec09980c47fdf216e242c2e3dd273595b3
cristian100 <cristian100@gmail.com>
2017-03-12 16:56:57 -0700

    Modified ads format.

---
+++ sites/all/themes/nel/templates/ads/html--fbia_sync--adslot.tpl.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<?php if (omega_extension_enabled('compatibility') && omega_theme_get_setting('omega_conditional_classes_html', TRUE)): ?>
+  <!--[if IEMobile 7]><html class="no-js ie iem7" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $more; ?>><![endif]-->
+  <!--[if lte IE 6]><html class="no-js ie lt-ie9 lt-ie8 lt-ie7" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $more; ?>><![endif]-->
+  <!--[if (IE 7)&(!IEMobile)]><html class="no-js ie lt-ie9 lt-ie8" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $more; ?>><![endif]-->
+  <!--[if IE 8]><html class="no-js ie lt-ie9" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $more; ?>><![endif]-->
+  <!--[if (gte IE 9)|(gt IEMobile 7)]><html class="no-js ie" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?><?php print $more; ?>><![endif]-->
+  <!--[if !IE]><!--><html class="no-js" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?><?php print $more; ?>><!--<![endif]-->
+<?php else: ?>
+  <html class="no-js" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?><?php print $more; ?>>
+<?php endif; ?>
+<head>
+  <title><?php print $head_title; ?></title>
+  <?php print $styles; ?>
+</head>
+<body>
+  <?php print $page; ?>
+</body>
+</html>

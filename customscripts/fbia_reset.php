8dcf5f9fb9cf68aefda88ad6443bbb0bc7f55ff6
cristian100 <cristian100@gmail.com>
2017-03-18 09:55:23 -0700

    Fixed code to allow keep processing more stuff.

---
+++ customscripts/fbia_reset.php
@@ -0,0 +1,10 @@
+<?php
+define('DRUPAL_ROOT', getcwd());
+$_SERVER['REMOTE_ADDR'] = "localhost"; // Necessary if running from command line
+require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
+drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+$GLOBALS['base_url'] = 'http://www.nayaritenlinea.mx';
+$module_path = DRUPAL_ROOT . '/' . drupal_get_path('module', 'fbia_sync') . '/inc/api.inc';
+require_once $module_path;
+fbia_sync_reset_all();
+

6e6d107d84f19b3bb641f37221b8a439965e2fbc
cristian100 <cristian100@gmail.com>
2017-05-12 12:24:41 -0600

    Added class handler for candidatos.

--- sites/all/modules/custom/nelads/js/nelads-dc-article_body.js
+++ sites/all/modules/custom/nelads/js/nelads-dc-article_body.js
@@ -2,7 +2,10 @@ googletag.cmd.push(function() {
   googletag.defineSlot('/15651346/Article-Body-300x250', [300, 250], 'div-gpt-ad-1446621104256-0').addService(googletag.pubads());
   // Start Custom Criteria
   google_class = document.getElementsByTagName('body')[0].getAttribute('data-google-class');
-  googletag.pubads().setTargeting("google_class",google_class);
+  googletag.pubads().setTargeting("google_class", google_class);
+  // Candidato class.
+  candidato_class = document.getElementsByTagName('body')[0].getAttribute('data-candidato-class');
+  googletag.pubads().setTargeting("candidato_class", candidato_class);
   // End Custom Criteria
   googletag.pubads().enableSingleRequest();
   googletag.enableServices();
